55d349350987d2c93acaa4f1066882a9a56e8700
maciejw <mwojcinski@gmail.com>
2022-04-01 21:51:24 +0200

    adding login and dashboard

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,14 +1,13 @@
 import React, {useRef, useState} from 'react';
 import { Form, Button, Card, Alert } from "react-bootstrap";
 import {useAuth} from '../contexts/AuthContext';
-import {Link, useNavigate} from "react-router-dom";
+import {Link, useNavigate } from "react-router-dom";
 
 
-export function Signup() {
+export function Login() {
     const emailRef = useRef();
     const passwordRef = useRef();
-    const passwordConfirmRef = useRef();
-    const {signup} = useAuth();
+    const {login} = useAuth();
     const [error, setError] = useState("");
     const [loading, setLoading] = useState(false);
     const navigate = useNavigate();
@@ -16,17 +15,13 @@ export function Signup() {
     async function handleSubmit(event) {
         event.preventDefault();
 
-        if (passwordRef.current.value !== passwordConfirmRef.current.value) {
-            return setError("Passwords do not match")
-        }
-        
         try {
             setError("");
             setLoading(true);
-          await  signup(emailRef.current.value, passwordRef.current.value);
+            await  login(emailRef.current.value, passwordRef.current.value);
             navigate("/");
         } catch {
-            setError("Failed to create an account");
+            setError("Failed to sign in");
         }
         setLoading(false);
     }
@@ -35,7 +30,7 @@ export function Signup() {
         <>
             <Card>
                 <Card.Body>
-                    <h2 className="text-center mb-4">Sign Up</h2>
+                    <h2 className="text-center mb-4">Log In</h2>
                     {error && <Alert variant="danger">{error}</Alert>}
                     <Form onSubmit={handleSubmit}>
                         <Form.Group id="email">
@@ -46,19 +41,15 @@ export function Signup() {
                             <Form.Label>Password</Form.Label>
                             <Form.Control type="password" ref={passwordRef} required />
                         </Form.Group>
-                        <Form.Group id="password-confirm">
-                            <Form.Label>Password Confirmation</Form.Label>
-                            <Form.Control type="password" ref={passwordConfirmRef} required />
-                        </Form.Group>
                         <Button disabled={loading} className="w-100" type="submit"
-                        style={{marginTop: "20px"}}>
-                            Sign Up
+                                style={{marginTop: "20px"}}>
+                            Log In
                         </Button>
                     </Form>
                 </Card.Body>
             </Card>
             <div className="w-100 text-center mt-2">
-                Already have an account? <Link to="/login">Log In</Link>
+                Need an account? <Link to="/signup">Sign Up</Link>
             </div>
 
         </>
